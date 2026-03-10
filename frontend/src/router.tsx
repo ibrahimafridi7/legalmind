f087aa628392c2e068e175b5e6e4b260d300bd7a
Muhammad Ibrahim <96227545+ibrahimafridi7@users.noreply.github.com>
2026-03-11 02:42:41 +0500

    fix: router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
 import { ChatPage } from './pages/ChatPage'
 import { DocumentsPage } from './pages/DocumentsPage'
 import { AuditLogsPage } from './pages/AuditLogsPage'
 import { ProtectedRoute } from './components/auth/ProtectedRoute'
+
+/** Redirect to /chat but keep ?code=...&state=... so Auth0 callback can finish before ProtectedRoute runs */
+function DefaultRedirect() {
+  const location = useLocation()
+  const to = `/chat${location.search}`
+  return <Navigate to={to} replace />
+}
 
 export const AppRouter = () => (
   <BrowserRouter>
@@ -33,7 +40,7 @@ export const AppRouter = () => (
           </ProtectedRoute>
         }
       />
-      <Route path="*" element={<Navigate to="/chat" replace />} />
+      <Route path="*" element={<DefaultRedirect />} />
     </Routes>
   </BrowserRouter>
 )
